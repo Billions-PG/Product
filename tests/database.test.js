526ffd79a61436d142de1ee7d5784a9965220106
Joey Huntington <jwhunt19@gmail.com>
2021-01-14 14:25:05 -0500

    Complete basic connection and data structure tests for database

--- tests/database.test.js
+++ tests/database.test.js
@@ -1,67 +1,84 @@
 const mongoose = require('mongoose');
 const Seller = require('../database/database.js');
 
+const dbName = 'sellers';
+
+beforeAll(async () => {
+  const url = `mongodb://localhost/${dbName}`;
+  await mongoose.disconnect();
+  await mongoose.connect(url, { useNewUrlParser: true });
+});
+
+afterAll(() => mongoose.disconnect());
+
 describe('Database Tests', () => {
-  const dbName = 'sellers';
-
-  beforeAll(async () => {
-    const url = `mongodb://localhost/${dbName}`;
-    await mongoose.disconnect();
-    await mongoose.connect(url, { useNewUrlParser: true });
-  });
-
-  afterAll(() => mongoose.disconnect());
-
-  async function removeAllCollections() {
-    Object.keys(mongoose.connection.collections).forEach((collection) => {
-      const col = mongoose.connection.collections[collection];
-      col.deleteMany();
-    });
-  }
-
-  afterEach(async () => {
-    await removeAllCollections();
-  });
-
   it('Should connect to the \'sellers\' database', async () => {
     expect(mongoose.connection.name).toBe('sellers');
   });
 
-  it('Should have correct data structure', async () => {
-    await Seller.create({
-      name: 'joey',
-      sales: 1234,
-      products: [
-        {
-          id: 1,
-          name: 'hat',
-          description: 'its a hat',
-          price: 9.99,
-          stock: 123,
-          sizes: false,
-          rating: 4.35,
-        },
-        {
-          id: 2,
-          name: 'shirt',
-          description: 'its a shirt',
-          price: 99.99,
-          stock: 123,
-          sizes: false,
-          rating: 3.5,
-        },
-      ],
-    });
-    const results = await Seller.find({});
-    expect(typeof results[0].name).toBe('string');
-    expect(typeof results[0].sales).toBe('number');
-    expect(Array.isArray(results[0].products)).toBe(true);
-    expect(typeof results[0].products[0].id).toBe('number');
-    expect(typeof results[0].products[0].name).toBe('string');
-    expect(typeof results[0].products[0].description).toBe('string');
-    expect(typeof results[0].products[0].price).toBe('number');
-    expect(typeof results[0].products[0].stock).toBe('number');
-    expect(typeof results[0].products[0].sizes).toBe('boolean');
-    expect(typeof results[0].products[0].rating).toBe('number');
+  it('Should have a sellers collection', () => {
+    expect(mongoose.connection.collections).toHaveProperty('sellers');
+  });
+});
+
+describe('Data Structure', () => {
+  it('Should have a seller name', async () => {
+    const results = await Seller.findOne();
+    expect(results).toHaveProperty('name');
+    expect(typeof results.name).toBe('string');
+  });
+
+  it('Should have a seller sales total', async () => {
+    const results = await Seller.findOne();
+    expect(results).toHaveProperty('sales');
+    expect(typeof results.sales).toBe('number');
+  });
+
+  it('Should have a seller product array', async () => {
+    const results = await Seller.findOne();
+    expect(results).toHaveProperty('products');
+    expect(Array.isArray(results.products)).toBe(true);
+  });
+
+  it('Should have id property in objects within products array', async () => {
+    const results = await Seller.findOne();
+    expect(results.products[0]).toHaveProperty('id');
+    expect(typeof results.products[0].id).toBe('number');
+  });
+
+  it('Should have name property in objects within products array', async () => {
+    const results = await Seller.findOne();
+    expect(results.products[0]).toHaveProperty('name');
+    expect(typeof results.products[0].name).toBe('string');
+  });
+
+  it('Should have description property in objects within products array', async () => {
+    const results = await Seller.findOne();
+    expect(results.products[0]).toHaveProperty('description');
+    expect(typeof results.products[0].description).toBe('string');
+  });
+
+  it('Should have price property in objects within products array', async () => {
+    const results = await Seller.findOne();
+    expect(results.products[0]).toHaveProperty('price');
+    expect(typeof results.products[0].price).toBe('number');
+  });
+
+  it('Should have stock property in objects within products array', async () => {
+    const results = await Seller.findOne();
+    expect(results.products[0]).toHaveProperty('stock');
+    expect(typeof results.products[0].stock).toBe('number');
+  });
+
+  it('Should have sizes property in objects within products array', async () => {
+    const results = await Seller.findOne();
+    expect(results.products[0]).toHaveProperty('sizes');
+    expect(typeof results.products[0].sizes).toBe('boolean');
+  });
+
+  it('Should have rating property in objects within products array', async () => {
+    const results = await Seller.findOne();
+    expect(results.products[0]).toHaveProperty('rating');
+    expect(typeof results.products[0].rating).toBe('number');
   });
 });
