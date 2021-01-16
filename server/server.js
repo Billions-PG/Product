0f0de005b77e85b0ba20dc3dec7148c4392b6265
Joey Huntington <jwhunt19@gmail.com>
2021-01-16 01:07:03 -0500

    Add route for specific id and tests

--- server/server.js
+++ server/server.js
@@ -5,15 +5,27 @@ const app = express();
 
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('hello from the server!');
-});
-
 app.get('/sellers', (req, res) => {
   Seller.find({}, (err, results) => {
     if (err) res.status(400).send(err);
     else res.status(200).send(results);
   });
+});
+
+app.get('/sellers/:prodId', async (req, res) => {
+  const sellers = await Seller.find({});
+  let notFound = true;
+  sellers.forEach((seller) => {
+    seller.products.forEach((product) => {
+      if (product.id === Number(req.params.prodId)) {
+        notFound = false;
+        res.status(200).send(seller);
+      }
+    });
+  });
+  if (notFound) {
+    res.status(404).send('Not Found');
+  }
 });
 
 module.exports = app;
--- tests/server.test.js
+++ tests/server.test.js
@@ -9,11 +9,57 @@ afterAll((done) => {
   done();
 });
 
-describe('Connection Tests', () => {
-  it('Gets the test endpoint', async (done) => {
+describe('Connection', () => {
+  it('Should return status code 200', async (done) => {
     const res = await request.get('/sellers');
     expect(res.status).toBe(200);
-    expect(typeof res.body).toBe('object');
+    done();
+  });
+
+  it('Should not return null', async (done) => {
+    const res = await request.get('/sellers');
+    expect(res.body).not.toBe(null);
+    done();
+  });
+});
+
+describe('GET', () => {
+  it('Should return an array of objects', async (done) => {
+    const res = await request.get('/sellers');
+    expect(Array.isArray(res.body)).toBe(true);
+    expect(typeof res.body[0]).toBe('object');
+    done();
+  });
+
+  it('Should return sellers with correct properties', async (done) => {
+    const res = await request.get('/sellers');
+    const rand = (max) => Math.floor(Math.random() * Math.floor(max));
+    expect(res.body[rand(res.body.length)]).toHaveProperty('name');
+    expect(res.body[rand(res.body.length)]).toHaveProperty('sales');
+    expect(res.body[rand(res.body.length)]).toHaveProperty('products');
+    const product = res.body[rand(res.body.length)].products[0];
+    if (product !== undefined) {
+      expect(product).toHaveProperty('id');
+      expect(product).toHaveProperty('name');
+      expect(product).toHaveProperty('image');
+      expect(product).toHaveProperty('description');
+      expect(product).toHaveProperty('price');
+      expect(product).toHaveProperty('stock');
+      expect(product).toHaveProperty('sizes');
+      expect(product).toHaveProperty('rating');
+    }
+    done();
+  });
+
+  it('Should return seller from valid product id', async (done) => {
+    const res = await request.get('/sellers/50');
+    expect(Object.keys(res.body).length).toBeGreaterThan(0);
+    done();
+  });
+
+  it('Should return nothing for invalid product id', async (done) => {
+    const res = await request.get('/sellers/500');
+    expect(Object.keys(res.body)).toHaveLength(0);
     done();
   });
 });
