af6ab96248e30ff06dc8010092ffedb2a7174609
chris <cmayer153@gmail.com>
2021-02-04 23:15:04 -0700

    postgres schema made. 1 record testing file. working through rewriting base queries

---
+++ db_postgres/product_schema.sql
@@ -0,0 +1,26 @@
+CREATE TABLE sellers (
+  _seller_id serial PRIMARY KEY,
+  name VARCHAR ( 50 ) NOT NULL,
+  numSales INT NOT NULL
+);
+
+CREATE TABLE products (
+  _product_id serial PRIMARY KEY,
+  seller_id INT NOT NULL,
+  FOREIGN KEY (seller_id)
+    REFERENCES sellers (_seller_id),
+  name VARCHAR (255) NOT NULL,
+  description VARCHAR (255) NOT NULL,
+  price NUMERIC NOT NULL,
+  stock INT NOT NULL,
+  sizes BOOLEAN NOT NULL,
+  rating NUMERIC(2,1) NOT NULL
+);
+
+CREATE TABLE photos (
+  _photo_id serial PRIMARY KEY,
+  product_id INT NOT NULL,
+  FOREIGN KEY (product_id)
+    REFERENCES products (_product_id),
+  url VARCHAR (255) NOT NULL
+);
---
+++ db_postgres/test_data.sql
@@ -0,0 +1,7 @@
+INSERT INTO sellers (name, numSales) VALUES ('DT Suzuki', '1949');
+INSERT INTO products (seller_id, name, description, price, stock, sizes, rating) \
+  VALUES ('1', 'Essays in Zen Buddhism', 'Zen as Chinese interpretation of the Doctrine of Enlightenment', '18.00', '10', 'false', '3.9');
+INSERT INTO photos (product_id, url) VALUES ('1', 'exterior1.jpg');
+INSERT INTO photos (product_id, url) VALUES ('1', 'interior1.jpg');
+INSERT INTO photos (product_id, url) VALUES ('1', 'exterior2.jpg');
+INSERT INTO photos (product_id, url) VALUES ('1', 'interior2.jpg');
